7ff7dfeb8e6ad1f98e0d4505cc50789ab2e93441
PreethamSub <91767887+PreethamSub@users.noreply.github.com>
2022-10-29 21:35:10 +0530

    Removed redundant data

--- stocks/stocks/spiders/stock.py
+++ stocks/stocks/spiders/stock.py
@@ -2,12 +2,9 @@ import scrapy
 import pandas as pd
 from stocks.items import StocksItem
 
-
-
 class QuotesSpider(scrapy.Spider):
     name = "stocks_1"
     
-
     def start_requests(self):
       urls = []
       data = pd.read_csv('/content/drive/MyDrive/Scrapy proj/stocks/stocks/spiders/EQUITY_L.csv')
@@ -21,32 +18,20 @@ class QuotesSpider(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        items = StocksItem()
-        keys = [
-            'CompanyName',
-            'BUYTriggerPrice',
-            'BUYStopLoss',
-            'BUYStockTarget1',
-            'BUYStockTarget2',
-            'SELLTriggerPrice',
-            'SELLStopLoss',
-            'SELLStockTarget1',
-            'SELLStockTarget2'
-            ]
+        data = StocksItem()
         values=[]
         values.append(response.css('h1.font-weight-normal.text-secondary span::text').get())
         for item in response.css('div.col-6.col-md-6.col-lg-3.p-1.font-weight-bold'):
             values.append(item.css('ul.list-group li.list-group-item.p-1.h4::text').get())
 
-        items['CompanyName'] = values[0]
-        items['BUYTriggerPrice'] = values[1]
-        items['BUYStopLoss'] = values[2]
-        items['BUYStockTarget1'] = values[3]
-        items['BUYStockTarget2'] = values[4]
-        items['SELLTriggerPrice'] = values[5]
-        items['SELLStopLoss'] = values[6]
-        items['SELLStockTarget1'] = values[7]
-        items['SELLStockTarget2'] = values[8]
+        data['CompanyName'] = values[0]
+        data['BUYTriggerPrice'] = values[1]
+        data['BUYStopLoss'] = values[2]
+        data['BUYStockTarget1'] = values[3]
+        data['BUYStockTarget2'] = values[4]
+        data['SELLTriggerPrice'] = values[5]
+        data['SELLStopLoss'] = values[6]
+        data['SELLStockTarget1'] = values[7]
+        data['SELLStockTarget2'] = values[8]
 
-        yield items
-
+        yield data
